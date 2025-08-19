4fd8d9804d3cb41d95f2d0285d7583eb68f822d5
Muhammad Faishal Darma Putra <61378935+mfaishaldp@users.noreply.github.com>
2025-08-19 11:47:41 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+Please create an array/list of numbers from 1 to 100. Print all these numbers in reverse order, but with the following rules :
+
+Do not print prime numbers.
+Replace numbers divisible by 3 with the text "Foo."
+Replace numbers divisible by 5 with the text "Bar."
+Replace numbers divisible by both 3 and 5 with the text "FooBar."
+Print the numbers horizontally, not vertically.
---
+++ result.py
@@ -0,0 +1,25 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+numbers = list(range(1, 101))
+numbers.reverse()
+
+result = []
+for n in numbers:
+    if is_prime(n):
+        continue
+    if n % 15 == 0:
+        result.append("FooBar")
+    elif n % 3 == 0:
+        result.append("Foo")
+    elif n % 5 == 0:
+        result.append("Bar")
+    else:
+        result.append(str(n))
+
+print(" ".join(result))
